ead73692024ad67cbfdb7033f766206d71da3f6b
Facundo <facundo.bo@hotmail.com>
2021-12-21 13:18:49 -0300

    avance start casi terminada

--- src/components/Start.js
+++ src/components/Start.js
@@ -19,21 +19,28 @@ const Start = () => {
      */
 
     return (
-        <div className="container-start">
-                    <LogoAction width={150} height={75} id="logo-start"/>
-                    {!isAuthenticated ? <Button onClick={() => authenticate({signingMessage: "Action Fintech Authentication"})} id="start-connect"><p>Connect Wallet</p></Button> : redirect() }
-            <div id="circulo-container">
-                <div className="circulo-container">
-                    <BlockchainDesign id="blockchain-design" width={600} height={600}/>
-                    <FondoRedondo className="fondo-redondo" width={700} height={700}/>
+        <>
+            <div className="container-start">
+                <LogoAction width={150} height={75} id="logo-start" />
+                {!isAuthenticated ? <Button onClick={() => authenticate({ signingMessage: "Action Fintech Authentication" })} id="start-connect"><p>CONNECT WALLET</p></Button> : redirect()}
+                <div id="circulo-container">
+                    <div className="circulo-container">
+                        <BlockchainDesign id="blockchain-design" width={600} height={600} />
+                        <FondoRedondo className="fondo-redondo" width={700} height={700} />
+                    </div>
                 </div>
             </div>
-            <div>
-                <div>
-                 
-                </div>
+
+            <div id="texto-start">
+                <h1 id="acceso-nft">
+                    Acceso a NFT de
+                    <br />
+                    contratos
+                </h1>
+
+                <Button onClick={() => authenticate({ signingMessage: "Action Fintech Authentication" })} id="login-start"><p id="login-text">LOGIN</p></Button>
             </div>
-        </div>
+        </>
     )
 }
 
