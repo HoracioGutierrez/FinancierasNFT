eda7ee57186be3b2f752248b2b974098e3562f21
tomicorsico <95648086+tomicorsico@users.noreply.github.com>
2021-12-22 12:38:57 -0300

    Update mint.js

--- mint.js
+++ mint.js
@@ -1,55 +1,33 @@
-Moralis.initialize("UBLBJztkt7wqWAiPaHmaTfZs5Kj8CoZs9wNn6erg")
-Moralis.serverURL = "https://ezdnxz4vljsr.usemoralis.com:2053/server"
-const CONTRACT_ADDRESS = "0x96ec78B9D9F36e38e18b5C35CB47b2CAA41D5498"
-let web3
 
+/** Connect to Moralis server */
+const serverUrl = "https://ezdnxz4vljsr.usemoralis.com:2053/server";
+const appId = "UBLBJztkt7wqWAiPaHmaTfZs5Kj8CoZs9wNn6erg";
+Moralis.start({ serverUrl, appId });
 
-async function init(){
-    let currentUser = Moralis.User.current();
-    console.log(currentUser.attributes.ethAddress)// El addrres devuelve null
-    if(!currentUser){
-        window.location.pathname = "/index.html" //Se puede usar esta misma logica para los roles
-    }
-    web3 = await Moralis.enableWeb3();
-    if (!chequearRol()){
-        console.log("no esta en rol")
-        window.location.pathname = "/index.html"
-    }
-    else {
-        console.log("esta en rol")
-    }
-    let accounts = await web3.eth.getAccounts();
+/** Add from here down */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+   try {
+      user = await Moralis.authenticate({ signingMessage: "Hello World!" })
+      console.log(user)
+      console.log(user.get('ethAddress'))
+      console.log("Ha iniciado sesion")
+      redirect()     
+   } catch(error) {
+     console.log(error)
+   }
+  }
 }
 
-async function add(currentUser){
-    let address = document.getElementById("address").value
-    console.log(address)
-    let id = parseInt(document.getElementById("id").value)
-    console.log(id)
-    const contract = new web3.eth.Contract(contractAbi, CONTRACT_ADDRESS);
-    const accounts = await web3.eth.getAccounts();
-    contract.methods.agregarFinanciera(address, id).send({from: currentUser.attributes.ethAddress})
+function redirect(){
+  location.href = "./mint.html";
 }
 
-async function chequearRol(currentUserbi){
-    
-    // const options = {
-    //     contractAddress: CONTRACT_ADDRESS,
-    //     functionName: "isInRole"
-    //     abi: contractAbi,
-    //     params: {
-    //         _address: currentUser.address,
-    //     },
-    // }
-    //const isInRole = await Moralis.executeFunction(options)
-    //console.log(isInRole)
-    const contract = new web3.eth.Contract(contractAbi, CONTRACT_ADDRESS);
-    const isAdmin = await contract.methods.isInRole(currentUser.attributes.ethAddress).call()
-    return isInRole
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
 }
 
-
-document.getElementById("send").onclick = add;
-
-init();
- 
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
