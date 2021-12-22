87e7a0c13699e51abf4d36c07071cae200c8af44
tomicorsico <95648086+tomicorsico@users.noreply.github.com>
2021-12-22 12:41:02 -0300

    Update mint.js

--- mint.js
+++ mint.js
@@ -1,33 +1,41 @@
+Moralis.serverURL = "https://ezdnxz4vljsr.usemoralis.com:2053/server"
+Moralis.initialize("UBLBJztkt7wqWAiPaHmaTfZs5Kj8CoZs9wNn6erg")
+const CONTRACT_ADDRESS = "0x96ec78B9D9F36e38e18b5C35CB47b2CAA41D5498"
+let web3
 
-/** Connect to Moralis server */
-const serverUrl = "https://ezdnxz4vljsr.usemoralis.com:2053/server";
-const appId = "UBLBJztkt7wqWAiPaHmaTfZs5Kj8CoZs9wNn6erg";
-Moralis.start({ serverUrl, appId });
-
-/** Add from here down */
-async function login() {
-  let user = Moralis.User.current();
-  if (!user) {
-   try {
-      user = await Moralis.authenticate({ signingMessage: "Hello World!" })
-      console.log(user)
-      console.log(user.get('ethAddress'))
-      console.log("Ha iniciado sesion")
-      redirect()     
-   } catch(error) {
-     console.log(error)
-   }
-  }
+async function init(){
+    let currentUser = Moralis.User.current();
+    console.log(currentUser.attributes.ethAddress)
+    web3 = await Moralis.enableWeb3();
+    
+    if(!currentUser || !await chequearRol(currentUser)){                 
+        window.location.pathname = "/index.html"  
+    }
+    else {
+        console.log("esta en rol")
+    }
 }
 
-function redirect(){
-  location.href = "./mint.html";
+async function add(){
+    let currentUser = Moralis.User.current(); 
+    
+    let address = document.getElementById("address").value
+    console.log(address)
+    let id = parseInt(document.getElementById("id").value)
+    console.log(id)
+
+    const contract = new web3.eth.Contract(contractAbi, CONTRACT_ADDRESS);
+    contract.methods.agregarFinanciera(address, id).send({from: currentUser.attributes.ethAddress}).then(function(receipt){
+        console.log(receipt)  // cuando se confirma la transaccion devuelve un json con el numero de trasacc, nro de bloque, gas, etc.
+    });
 }
 
-async function logOut() {
-  await Moralis.User.logOut();
-  console.log("logged out");
+async function chequearRol(currentUser){
+    const contract = new web3.eth.Contract(contractAbi, CONTRACT_ADDRESS);
+    const isAdmin = await contract.methods.isInRole(currentUser.attributes.ethAddress).call()
+    return isAdmin
 }
 
-document.getElementById("btn-login").onclick = login;
-document.getElementById("btn-logout").onclick = logOut;
+document.getElementById("send").onclick = add;
+
+init();
