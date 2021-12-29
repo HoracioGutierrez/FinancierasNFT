9e77dbf4363b617f5366e0a878c8a33925140851
manupaw <manupaw2000@gmail.com>
2021-12-29 11:20:32 -0300

    cosas raritas en testmint

--- MintearTest/Program.cs
+++ MintearTest/Program.cs
@@ -3,6 +3,7 @@ using Ipfs.Http;
 using Nethereum.ABI.FunctionEncoding.Attributes;
 using Nethereum.Contracts;
 using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.Util;
 using Nethereum.Web3;
 using Nethereum.Web3.Accounts;
 using Newtonsoft.Json;
@@ -11,6 +12,9 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -61,6 +65,8 @@ namespace MintearTest
             //PDF TO IPFS
             var pdf = File.ReadAllBytes(@"Files\contrato.pdf");
 
+            var response = CallAPIAsync(pdf);
+
             var pdfHash = subirIpfs(pdf);
 
             Console.WriteLine("Pdf IPFS Hash: " + pdfHash);
@@ -108,10 +114,71 @@ namespace MintearTest
 
             var recipient = "0x87d43F463118cbcaC8Cf9F31f2C824dE02C3AD8E"; //Address financiera
 
+
+            var balance = GetBalance(metadata, recipient, jsonHash);
+
             mintearNft(metadata, recipient, jsonHash);
 
             Console.WriteLine("Finish!");
             Console.ReadKey();
+        }
+
+        private static string CallAPIAsync(byte[] pdf)
+        {
+            //using (var httpClient = new HttpClient())
+            //{
+            //    using (var form = new MultipartFormDataContent())
+            //    {
+            //        using (var fs = File.OpenRead(path))
+            //        {
+            //            using (var streamContent = new StreamContent(fs))
+            //            {
+            //                using (var fileContent = new ByteArrayContent(await streamContent.ReadAsByteArrayAsync()))
+            //                {
+            //                    fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+
+            //                    // "file" parameter name should be the same as the server side input parameter name
+            //                    form.Add(fileContent, "file", Path.GetFileName(path));
+            //                    HttpResponseMessage response = await httpClient.PostAsync(@"https://ipfs.infura.io:5001/api/v0/add", form);
+
+            //                    return response;
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            using (var multiPartStream = new MultipartFormDataContent())
+            {
+                multiPartStream.Add(new StringContent("{}"), "metadata");
+                multiPartStream.Add(new ByteArrayContent(pdf, 0, pdf.Length), "file", "pdfname.pdf");
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, @"https://ipfs.infura.io:5001/api/v0/add")
+                {
+                    Content = multiPartStream
+                };
+                ////"application /json" - content type
+                //request.Headers.Accept.Add(JSON_GENERIC_MEDIA_TYPE);
+                //request.Headers.Add("X-Client-Id", ClientId);
+                //request.Headers.Add("Tenant-Id", TenantId);
+
+                HttpCompletionOption option = HttpCompletionOption.ResponseContentRead;
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = ((sender, certificate, chain, sslPolicyErrors) => true);
+
+                HttpClient _httpclient = new HttpClient();
+
+                using (HttpResponseMessage response = _httpclient.SendAsync(request, option).Result)
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responsetext = response.Content.ReadAsStringAsync().Result;
+
+                        return responsetext;
+                    }
+                }
+
+            }//End Try
+
+            return "";
         }
 
         public static string subirIpfs(byte[] archivo)
@@ -146,11 +213,46 @@ namespace MintearTest
             {
                 newBitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
 
-                newBitmap.Save("logoEscrito.png");
+                //newBitmap.Save("logoEscrito.png");
                 newBitmap.Dispose();
 
                 return stream.ToArray();
             }
+        }
+
+        public static BigInteger GetBalance(Metadata metadata, string recipient, string tokenURI)
+        {
+            //INIT WEB3
+            var privateKey = "0x9628316996948cf66bed79e797f9a5737a456ce478777454d2bd9df250c03bc3";
+            var account = new Account(privateKey, Nethereum.Signer.Chain.Rinkeby);
+            var url = "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+            var web3 = new Web3(account, url);
+
+            var transferHandler = web3.Eth.GetContractTransactionHandler<MintNFTFunction>();
+
+            var mintNFTFunction = new MintNFTFunction
+            {
+                Recipient = recipient,  //Receptor del NFT
+                TokenURI = tokenURI,    //Json del nft 
+                Metadata = metadata    //Metadata publica  
+            };
+            
+            var estimate = transferHandler.EstimateGasAsync("0x3EFbab2e1675fd3397C28A8CC82Be4aee0d574E5", mintNFTFunction).Result;
+
+            var estimatedGas = Web3.Convert.ToWei(estimate.Value, UnitConversion.EthUnit.Gwei);
+
+            var balance = web3.Eth.GetBalance.SendRequestAsync("0x75322D04B275d720570a58BEAF53BaDe400B26c7").Result;
+
+            if(balance.Value > estimate.Value)
+            {
+                // :D
+            }
+            else
+            {
+                // :(
+            }
+
+            return balance.Value;
         }
 
         public static async void mintearNft(Metadata metadata, string recipient, string tokenURI)
