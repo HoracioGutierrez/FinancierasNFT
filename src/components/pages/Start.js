72e167b4a5942324f73083cc7be61925dfe680f2
Horacio Gutierrez <horacio.estevez@gmail.com>
2021-12-31 16:06:25 -0300

    Add public landing support with Auth hooks integrated

--- src/components/pages/Start.js
+++ src/components/pages/Start.js
@@ -1,7 +1,20 @@
+import { Button } from "react-bootstrap"
+import BlockchainDesign from "../../assets/BlockchainDesign"
+import FondoRedondo from "../../assets/FondoRedondo"
+import useAuth from "../hooks/useAuth"
+
 const Start = () => {
+
+    const {logInToMoralis} = useAuth()
+
     return (
-        <div>
-           soy start 
+        <div id="landing">
+           <BlockchainDesign id="blockchain-design"/>
+           <FondoRedondo className="fondo-redondo"/>
+           <div id="landing-banner">
+                <h1>Acceso a NFT</h1>
+                <Button id="landing-login" onClick={() => logInToMoralis()}><p>Login</p></Button>
+           </div>
         </div>
     )
 }
