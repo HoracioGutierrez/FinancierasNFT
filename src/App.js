cc9b74ac9bafdb03bd188cc15e1c2f38c5d5fc1a
Horacio Gutierrez <horacio.estevez@gmail.com>
2021-12-31 23:57:32 -0300

    Start refactoring css into scss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import { MetaMaskProvider } from "metamask-react";
 import { MetamaskStateProvider } from "use-metamask";
-import Header from './components/layout/Header';
-import Main from './components/layout/Main';
 import AuthProvider from './components/providers/AuthProvider';
 import Web3Provider from './components/providers/Web3Provider';
 import Layout from './components/layout/Layout';
